9533da64a4e6f702dbeebeff0638e9766db119fe
Caleb Cauthon <calebcauthon@gmail.com>
2020-10-28 09:50:23 -0500

    Add a dashboard with dummy data

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get 'welcome/index'
+  get 'dashboard', to: 'welcome#dashboard'
   get 'questions', to: 'questions#questions'
   get 'question/:id', to: 'questions#question'
 end
