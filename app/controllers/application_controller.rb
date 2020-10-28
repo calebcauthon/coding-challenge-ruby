d815183cd8b7b1c785034307439ad86f92916a82
Caleb Cauthon <calebcauthon@gmail.com>
2020-10-27 23:54:35 -0500

    Include answers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   def questions
-    questions = Question.includes(:user).where :share => true
+    questions = Question.includes(:user, :answers).where :share => true
 
     return head :no_content unless questions.count > 0
 
-    render json: questions.to_json(include: :user)
+    render json: questions.to_json(include: [:user, :answers])
   end
 end
--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -26,4 +26,14 @@ class ApplicationControllerTest < ActionController::TestCase
     get :questions
     assert_equal 'asker', JSON.parse(response.body).first['user']['name']
   end
+
+  test "should return the answers to the question" do
+    question = Question.create!(share: true, title: 'question-4', user: User.new)
+    Answer.create!(body: 'answer-1', question: question, user: User.new)
+    Answer.create!(body: 'answer-2', question: question, user: User.new)
+
+    get :questions
+    assert_equal 'answer-1', JSON.parse(response.body).first['answers'][0]['body']
+    assert_equal 'answer-2', JSON.parse(response.body).first['answers'][1]['body']
+  end
 end
