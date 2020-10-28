6ee2a5ab9c980d2b9910dc54c3383f0fd84ca1e0
Caleb Cauthon <calebcauthon@gmail.com>
2020-10-28 09:40:42 -0500

    Refactor

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,23 +1,4 @@
 class QuestionsController < ApplicationController
-  def api_key_missing?
-    tenant = Tenant.where(id: params['tenant_id'].to_i).first
-    return true unless tenant && params['api_key'] == tenant.api_key
-  end
-
-  def forbidden_status
-    return render json: {}, status: :forbidden
-  end
-
-  def increment_api_request_count!
-    tenant = Tenant.where(id: params['tenant_id'].to_i).first
-    tenant.api_request_count = tenant.api_request_count + 1
-    tenant.save!
-  end
-
-  def render_question question_entity
-    render json: question_entity.to_json(include: [:user, :answers])
-  end
-
   def question
     return forbidden_status if api_key_missing?
     increment_api_request_count!
@@ -36,5 +17,26 @@ class QuestionsController < ApplicationController
     return head :no_content unless questions.count > 0
 
     render_question questions
+  end
+
+  private
+
+  def api_key_missing?
+    tenant = Tenant.where(id: params['tenant_id'].to_i).first
+    return true unless tenant && params['api_key'] == tenant.api_key
+  end
+
+  def forbidden_status
+    return render json: {}, status: :forbidden
+  end
+
+  def increment_api_request_count!
+    tenant = Tenant.where(id: params['tenant_id'].to_i).first
+    tenant.api_request_count = tenant.api_request_count + 1
+    tenant.save!
+  end
+
+  def render_question question_entity
+    render json: question_entity.to_json(include: [:user, :answers])
   end
 end
