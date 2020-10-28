4110c3b3a9d49b071180ceeec08093e938acab95
Caleb Cauthon <calebcauthon@gmail.com>
2020-10-27 23:49:03 -0500

    Include the user with the question

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   def questions
-    questions = Question.where :share => true
+    questions = Question.includes(:user).where :share => true
 
     return head :no_content unless questions.count > 0
 
-    render json: questions.to_json
+    render json: questions.to_json(include: :user)
   end
 end
--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -8,14 +8,22 @@ class ApplicationControllerTest < ActionController::TestCase
   end
 
   test "should return questions that are shareable" do
-    Question.create!(share: true, title: 'question', user: User.new)
+    Question.create!(share: true, title: 'question-1', user: User.new)
     get :questions
     assert_equal 1, JSON.parse(response.body).count
   end
 
   test "should *only* return questions that are shareable" do
-    Question.create!(share: false, title: 'question', user: User.new)
+    Question.create!(share: false, title: 'question-2', user: User.new)
     get :questions
     assert_response 204
+  end
+
+  test "should return the user that asked the question" do
+    user = User.create!(name: 'asker')
+    Question.create!(share: true, title: 'question-3', user_id: user.id)
+
+    get :questions
+    assert_equal 'asker', JSON.parse(response.body).first['user']['name']
   end
 end
