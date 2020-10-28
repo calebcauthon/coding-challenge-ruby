86acb62f30c814d60654b91b30357cd3bfea31c0
Caleb Cauthon <calebcauthon@gmail.com>
2020-10-27 23:37:35 -0500

    Only return shareable questions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   def questions
-    head :no_content
+    questions = Question.where :share => true
+
+    return head :no_content unless questions.count > 0
+
+    render json: questions.to_json
   end
 end
--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
+require 'json'
 
 class ApplicationControllerTest < ActionController::TestCase
   test "should return no content if there are no questions" do
+    get :questions
+    assert_response 204
+  end
+
+  test "should return questions that are shareable" do
+    Question.create!(share: true, title: 'question', user: User.new)
+    get :questions
+    assert_equal 1, JSON.parse(response.body).count
+  end
+
+  test "should not return questions that are not shareable" do
+    Question.create!(share: false, title: 'question', user: User.new)
     get :questions
     assert_response 204
   end
